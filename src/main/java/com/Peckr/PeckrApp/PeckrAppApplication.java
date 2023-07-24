7743a00b5ea9a9cf90612a5c68dcdc7c3bd027b3
SMLindsay <137032630+SMLindsay@users.noreply.github.com>
2023-07-24 15:45:27 +0100

    First commit

---
+++ src/main/java/com/Peckr/PeckrApp/PeckrAppApplication.java
@@ -0,0 +1,13 @@
+package com.Peckr.PeckrApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeckrAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeckrAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Peckr/PeckrApp/PeckrAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Peckr.PeckrApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeckrAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
